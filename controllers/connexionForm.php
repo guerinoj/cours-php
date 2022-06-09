eefd1cae2e4a74f08d6039f642057ff8b6a182c2
Jonathan Guerino <guerino.jonathan@gmail.com>
2022-06-09 10:42:09 +0200

    fonction login

--- controllers/connexionForm.php
+++ controllers/connexionForm.php
@@ -1,15 +1,30 @@
 <?php
 
+/**
+ * Fonction qui prend 2 paramètres : nom d'utilisateur et mot de passe
+ * Retourne soit l'utilisateur soit faux
+ */
+function login(string $login, string $pwd)
+{
+  if ($login == "moi" && $pwd == "123") {
+    //Je stocke en session l'utilisateur connecté
+    $_SESSION['user'] = $login;
+    return $login;
+  } else {
+    return false;
+  }
+}
+
+
+
 
 //Si je suis déjà connecté
-if ($_SESSION['user']) {
+if (isset($_SESSION['user'])) {
   include('pages/connexionFormConfirmation.php');
 } else {
-
+  $user = login($_POST['login'], $_POST['password']);
   //Si la connexion à réussie 
-  if ($_POST['login'] == "moi" && $_POST['password'] == "123") {
-    //Je stocke en session l'utilisateur connecté
-    $_SESSION['user'] = $_POST['login'];
+  if ($user) {
     //Je recharge la page pour afficher les bonnes informations dans le menu
     header("Location: index.php?page=connexionForm");
   } else {

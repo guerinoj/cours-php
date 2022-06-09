b7f53c3cf33fe3d976845659247adc9026d259cf
Jonathan Guerino <guerino.jonathan@gmail.com>
2022-06-09 09:59:05 +0200

    Amélioration du formulaire de connection

--- controllers/connexionForm.php
+++ controllers/connexionForm.php
@@ -1,14 +1,21 @@
 <?php
 
-//Si la connexion à réussie 
-if ($_POST['login'] == "moi" && $_POST['password'] == "123") {
-  $user = $_POST['login'];
-  // On crée une variable de session avec la clé user
-  $_SESSION['user'] = $user;
+
+//Si je suis déjà connecté
+if ($_SESSION['user']) {
   include('pages/connexionFormConfirmation.php');
 } else {
-  //Si la connexion à échouer, j'affiche un message d'erreur
-  echo '<div class="alert alert-warning" role="alert">
+
+  //Si la connexion à réussie 
+  if ($_POST['login'] == "moi" && $_POST['password'] == "123") {
+    //Je stocke en session l'utilisateur connecté
+    $_SESSION['user'] = $_POST['login'];
+    //Je recharge la page pour afficher les bonnes informations dans le menu
+    header("Location: index.php?page=connexionForm");
+  } else {
+    //Si la connexion à échouer, j'affiche un message d'erreur
+    echo '<div class="alert alert-warning" role="alert">
     Erreur de connexion...
     </div>';
+  }
 }
--- pages/connexionFormConfirmation.php
+++ pages/connexionFormConfirmation.php
@@ -1,4 +1,4 @@
-<h1>Salut à toi <?= $user; ?> !</h1>
+<h1>Salut à toi <?= $_SESSION['user']; ?> !</h1>
 
 <div class="container">
   <p>Tu es bien connecté :-D</p>
